af87196783534575f5150b447f25a4ecd11a7c4a
ton1o1 <ton1o1@users.noreply.github.com>
2015-12-02 16:48:49 +0100

    réparation js

--- inc/footer.php
+++ inc/footer.php
@@ -4,5 +4,14 @@
 
 <script src="/H-G/js/jquery.min.js"></script>
 <script src="/H-G/js/easyResponsiveTabs.js" type="text/javascript"></script>
+<script>
+$(document).ready(function () {
+    $('#horizontalTab').easyResponsiveTabs({
+        type: 'default', //Types: default, vertical, accordion           
+        width: 'auto', //auto or any width like 600px
+        fit: true   // 100% fit in a container        
+    });
+});
+</script>
 </body>
 </html>
