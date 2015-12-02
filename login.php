e361979efe639b4ff313d70bd5c63f29cfa26875
ton1o1 <ton1o1@users.noreply.github.com>
2015-12-02 17:29:19 +0100

    login form

--- login.php
+++ login.php
@@ -1,16 +1,22 @@
 <?php
-
-$error = '';
+require_once 'pdo.php';
 
 if ( $_POST ) {
 
-	if ( !empty($_POST['action']['connexion']) ) {
+	session_start();
+
+	// Sauvegarde temporaire des valeurs saisies par l'user
+	foreach($_POST['login'] as $key => $value){
+		$_SESSION['form']['login'][$key] = $value;
+	}
+
+	if ( !empty($_POST['login']['submit']) ) {
 		
 		if ( !empty($_POST['login']['email']) && !empty($_POST['login']['password']) ) {
 
-			$pdo = include('conf/pdo.php');
-			$statement = $pdo->prepare('SELECT * FROM users WHERE email = :email;');
+			$statement = $pdo->prepare('SELECT * FROM :who WHERE email = :email;');
 			$statement->execute([
+				':who' => $_POST['login']['who'],
 				':email' => $_POST['login']['email'],
 			]);
 			$users = $statement->fetchAll();
@@ -23,13 +29,13 @@ if ( $_POST ) {
 					$_SESSION['auth'] = $users[0];
 					die(header('Location: ./' . ( !empty($_POST['action']['next']) ? $_POST['action']['next'] : '' ) ));
 
-				} else $error = "Mot de pase invalides !";
+				} else $_SESSION['error'] = "Mot de pase invalides !";
 
-			} else $error = "Email erroné.";
+			} else $_SESSION['error'] = "Email erroné.";
 
-		} else $error = "Tous les champs doivent être remplis.";
+		} else $_SESSION['error'] = "Tous les champs doivent être remplis.";
 
-	} else $error = "Le formulaire n'a pas été correctement validé.";
+	} else $_SESSION['error'] = "Le formulaire n'a pas été correctement validé.";
 
 }
 
