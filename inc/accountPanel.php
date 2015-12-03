51f0bc4d55bd7b292f1cea086f87da367255aaa1
ton1o1 <ton1o1@users.noreply.github.com>
2015-12-03 09:58:31 +0100

    llll

--- inc/accountPanel.php
+++
@@ -1,2 +0,0 @@
-<label class="radio"><input type="radio" name="whoIs" value="host"><i></i>Je suis un host</label>
-<label class="radio"><input type="radio" name="whoIs" value="guest"><i></i>Je suis un guest</label>
--- inc/form_login.php
+++ inc/form_login.php
@@ -1,11 +1,13 @@
 <div class="login-top">
-    <?php if ( $_SESSION['error'] ) { ?>
+    <?php if ( !empty($_SESSION['error']) ) { ?>
         <h2 style="color: red;"><?=$_SESSION['error']?></h2>
     <?php } ?>
     <form method="post" action="login.php">
         <input type="text" class="email" placeholder="Email" name="login['email']" value="<?=!empty($_SESSION['form']['login']['email']) ? $_SESSION['form']['login']['email'] : ''?>" required=""/>
         <input type="password" class="password" placeholder="Mot de passe" name="login['password']" value="<?=!empty($_SESSION['form']['login']['password']) ? $_SESSION['form']['login']['password'] : ''?>" required=""/>
-        <input type="text" name="who" value="" />
+        <br /><br />
+        <label class="radio"><input type="radio" name="who" value="host"><i></i>Je suis un host</label>
+        <label class="radio"><input type="radio" name="who" value="guest"><i></i>Je suis un guest</label>
         <div class="login-bottom login-bottom1">
             <div class="submit">
                 <input type="submit" name="login['submit']" value="CONNEXION"/>
